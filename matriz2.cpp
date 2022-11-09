0a801072043b005f06c17d7f06632ce97d19808f
Nicolas Marzagão <108955109+Nicolas-Marzagao@users.noreply.github.com>
2022-11-09 20:34:26 -0300

    Add files via upload

---
+++ matriz2.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() 
+{
+	// vars
+	char V_loja[4][30];
+	char V_prod[4][15];
+	
+	int  M_precos[4][4];
+	int  i, j;
+	
+	// Pegando Input
+	for (i=0; i<4; i++) {
+		printf("Digite o nome da loja[%d]", i);
+		gets(V_loja);
+	}	
+	
+	printf("")
+}
